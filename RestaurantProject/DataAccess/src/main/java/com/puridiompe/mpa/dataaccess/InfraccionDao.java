291466b1833803f64f65a72d127b28822d7c6c88
jzapana <j.zapana@puridiompe.com>
2015-08-22 20:46:16 -0500

    Dao classes

--- RestaurantProject/DataAccess/src/main/java/com/puridiompe/mpa/dataaccess/InfraccionDao.java
+++
@@ -1,5 +0,0 @@
-package com.puridiompe.mpa.dataaccess;
-
-public class InfraccionDao {
-
-}
---
+++ RestaurantProject/DataAccess/src/main/java/com/puridiompe/mpa/dataaccess/VehiculoDao.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.puridiompe.mpa.dataaccess;
+
+import com.puridiompe.mpa.business.general.dto.VehiculoDto;
+
+/**
+ * @author Johnny
+ *
+ */
+public interface VehiculoDao {
+
+	public VehiculoDto getVehiculoByPlaca(String placa);
+}
