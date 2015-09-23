c39d21b9d9f7059b93075f3c52a10c8789145c46
jordan <jordansalasc@gmail.com>
2015-09-23 16:27:50 -0500

    Modification of Sequence Generator of loginHistorial

--- RestaurantProject/Domain/src/main/java/com/puridiompe/mpa/sistran/domain/persistence/LoginHistorial.java
+++ RestaurantProject/Domain/src/main/java/com/puridiompe/mpa/sistran/domain/persistence/LoginHistorial.java
@@ -5,19 +5,22 @@ import java.util.Date;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 import org.hibernate.annotations.GenericGenerator;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Entity
 @Table(name = "\"TrPpe_LogHis\"")
 public class LoginHistorial {
 
 	@Id
-	@Column(name = "\"idloghist\"")
-	@GenericGenerator(name = "generator", strategy = "increment")
-	@GeneratedValue(generator = "generator")
+	@SequenceGenerator(name = "pk_sequence", sequenceName = "tr_loghis_id", allocationSize = 1)
+	@GeneratedValue(strategy = GenerationType.AUTO, generator = "pk_sequence")
+	@Column(name = "\"idloghist\"", unique=true, nullable=false)
 	private Integer idLogHist;
 	
 	@Column(name = "\"idUsuario\"", nullable = false)
