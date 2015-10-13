71447909939624f61c0556177fde8f10761c9842
Alison <alison.torres18@gmail.com>
2015-10-12 19:47:25 -0500

    # WARNING: head commit changed in the meantime
    Security by modules updated for users,

---
+++ RestaurantProject/RestController/src/main/java/com/puridiompe/mpa/rest/controller/seguridad/message/ApplicationResponse.java
@@ -0,0 +1,46 @@
+package com.puridiompe.mpa.rest.controller.seguridad.message;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.puridiompe.mpa.business.security.dto.PerfilDto;
+import com.puridiompe.mpa.common.rest.message.ResponseBody;
+
+
+public class ApplicationResponse extends ResponseBody {
+	
+	private List<String> roles;
+	
+	private Boolean registeredDevice;
+	
+	public ApplicationResponse(){
+		roles= new ArrayList<>();
+	}
+
+	public List<String> getRoles() {
+		return roles;
+	}
+	
+	
+	public void setPerfil(List<PerfilDto> perfiles) {
+		roles= new ArrayList<>();
+		for (PerfilDto perfil: perfiles){
+			this.roles.add(perfil.getAuthority());
+		}
+	}
+	
+	public void setRoles(List<String> roles) {
+		this.roles = roles;
+	}
+
+	public Boolean getRegisteredDevice() {
+		return registeredDevice;
+	}
+
+	public void setRegisteredDevice(Boolean registeredDevice) {
+		this.registeredDevice = registeredDevice;
+	}
+	
+	
+}
+
