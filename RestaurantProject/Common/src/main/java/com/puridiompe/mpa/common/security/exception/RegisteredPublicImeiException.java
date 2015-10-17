c72cd976096604be7ed9d33293944c5ee8c0834c
Alison <alison.torres18@gmail.com>
2015-10-17 16:28:12 -0500

    creation of a new exception to validate if imei is already registered

---
+++ RestaurantProject/Common/src/main/java/com/puridiompe/mpa/common/security/exception/RegisteredPublicImeiException.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package com.puridiompe.mpa.common.security.exception;
+
+/**
+ * @author puridiompe
+ *
+ */
+@SuppressWarnings("serial")
+public class RegisteredPublicImeiException extends SecurityException {
+
+	public RegisteredPublicImeiException(String message) {
+		super(message);
+	}
+
+}
