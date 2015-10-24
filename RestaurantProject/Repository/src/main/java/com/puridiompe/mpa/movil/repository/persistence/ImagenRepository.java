eadb18ac86cb1f22637b033897aabc2e2209627a
Ali <a.arapa@puridiompe.com>
2015-10-24 11:16:31 -0500

    New query to get images by reclamo

--- RestaurantProject/Repository/src/main/java/com/puridiompe/mpa/movil/repository/persistence/ImagenRepository.java
+++ RestaurantProject/Repository/src/main/java/com/puridiompe/mpa/movil/repository/persistence/ImagenRepository.java
@@ -11,5 +11,8 @@ public interface ImagenRepository  extends JpaRepository<Imagen, Integer>{
 	
 	@Query("from Imagen i where i.tipo = ?1")
 	public List<Imagen> findByTipo(String tipo);
+	
+	@Query("from Imagen i where i.idPadre = ?1")
+	public List<Imagen> findByidPadre(Integer idPadre);
 
 }
