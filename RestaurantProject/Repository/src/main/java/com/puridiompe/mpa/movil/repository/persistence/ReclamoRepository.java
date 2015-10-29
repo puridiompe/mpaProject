d5350b45333495e741b573905d8c94a3daa63652
Lucho <l.mendoza@puridiompe.com>
2015-10-29 12:10:52 -0500

    Fixes for reclamoDaoImpl and Repository

--- RestaurantProject/Repository/src/main/java/com/puridiompe/mpa/movil/repository/persistence/ReclamoRepository.java
+++ RestaurantProject/Repository/src/main/java/com/puridiompe/mpa/movil/repository/persistence/ReclamoRepository.java
@@ -18,9 +18,15 @@ public interface ReclamoRepository extends JpaRepository<Reclamo, Integer>{
 	@Query("from  Reclamo r where r.imei = ?1 ORDER BY dni DESC")
 	public List<Reclamo> findByImeiDni (String imei);
 	
-	@Query("from  Reclamo r ")//ORDER BY fecCre DESC")
+	@Query("from  Reclamo r ORDER BY idReclamo ASC")//ORDER BY fecCre DESC")
 	public List<Reclamo> findAll ();
 	
 	@Query("from  Reclamo r where r.idReclamo = ?1")
 	public Reclamo findById (Integer idReclamo);
+	
+	@Query("from  Reclamo r where r.estado != ?1 ORDER BY idReclamo ASC")
+	public List<Reclamo> findAllAvailable (Integer estado);
+	
+	@Query("from  Reclamo r where r.imei = ?1, AND r.estado != ?2 ORDER BY idReclamo DESC")
+	public List<Reclamo> findAvailableByImei (String imei, Integer estado);
 }
