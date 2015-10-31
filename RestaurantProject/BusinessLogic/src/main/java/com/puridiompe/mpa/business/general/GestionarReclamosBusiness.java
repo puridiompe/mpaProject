5730055271d3e7d9a14794b49d5f0d359afedca3
Lucho <l.mendoza@puridiompe.com>
2015-10-31 15:00:16 -0500

    Reclamos removed

--- RestaurantProject/BusinessLogic/src/main/java/com/puridiompe/mpa/business/general/GestionarReclamosBusiness.java
+++
@@ -1,9 +0,0 @@
-package com.puridiompe.mpa.business.general;
-
-import com.puridiompe.mpa.business.general.dto.ReclamosDto;
-import com.puridiompe.mpa.common.security.exception.SecurityException;
-
-public interface GestionarReclamosBusiness {
-
-	public ReclamosDto getReclamos(String imei) ;
-}
